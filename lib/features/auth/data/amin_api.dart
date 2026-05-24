61abad66bf117af60b59ecc215360c1b62bedae0
Palash Chandra Roy <90688529+Palash-Chandra-Roy@users.noreply.github.com>
2026-05-24 08:09:31 +0600

    update api link
    update api link
    
    Co-Authored-By: Shahed Ali <164641475+shahedroks@users.noreply.github.com>

---
+++ lib/features/auth/data/amin_api.dart
@@ -0,0 +1,24 @@
+class ApiConstants {
+  static const String baseUrl = "https://api.aican.co.il/backend";
+  static const String imageBaseUrl = "https://api.aican.co.il";
+
+  // Auth
+  static const String login = "/auth/login";
+  static const String register = "/auth/register";
+
+  static const String forgetPassword = "/auth/forgetPassword";
+
+  // User
+  static const String me = "/me";
+
+  // Devices
+  static const String myDevices = "/devices";
+  static const String adminDevices = "/admin/devices";
+
+  // Zones
+  static const String myZones = "/zones";
+  static const String adminZones = "/admin/zones";
+
+  // Categories
+  static const String myCategories = "/categories";
+}
